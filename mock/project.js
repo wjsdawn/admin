719ae6b38a5885a015f622e4af07c42576fb3f30
wjs_dawn <1666938053@qq.com>
2022-01-14 15:59:40 +0800

    代码规范

--- mock/project.js
+++ mock/project.js
@@ -3,7 +3,7 @@ const Mock = require('mockjs')
 const data = Mock.mock({
   'items|30': [{
     id: '@id',
-    'area|1': ['中心城区','科技城区','农旅融合社','临港东区','临港南区','临港西区'],
+    'area|1': ['中心城区', '科技城区', '农旅融合社', '临港东区', '临港南区', '临港西区'],
     property: '@ctitle',
     project: '@title'
   }]
